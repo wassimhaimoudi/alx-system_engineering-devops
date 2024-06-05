b774b9fdd7175fd149a8c5637ea85ac9667865c3
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-06-05 01:55:31 +0100

    Updated solution to advanced task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,26 @@
-# Using puppet, ensures nginx is installed and configurated so that http header response contains a custom header key/value pair with the name `X-Served-By` and the hostname as a value.
+# Using puppet, ensures nginx is installed and configurated
+# so that http header response contains a custom header key/value pair
+# with the name `X-Served-By` and the hostname as a value.
 
 exec { 'apt-get update':
   command => '/usr/bin/apt-get update',
   user    => 'root',
 }
 
-packege { 'nginx'
-  ensure => ensure, latest,
+package { 'nginx':
+  ensure => latest,
 }
 
-file_line {'custom_header'
+file_line { 'custom_header':
   ensure => present,
-  path => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line => "http {\n\tadd_header X-Served-By ${hostname};"
+  path   => '/etc/nginx/nginx.conf',
+  match  => 'http {',
+  line   => "http {\n\tadd_header X-Served-By ${facts['networking']['hostname']};",
+  notify => Exec['restart_nginx'],  # Notify the restart if the line is added/changed
 }
 
-exec {
-  command => '/usr/bin/service nginx restart',
-  user => 'root',
+exec { 'restart_nginx':
+  command     => '/usr/bin/service nginx restart',
+  user        => 'root',
+  refreshonly => true,  # Only run when notified
 }
