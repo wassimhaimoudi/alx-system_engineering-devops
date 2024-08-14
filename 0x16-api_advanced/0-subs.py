203d71d4f1d3c1bfd9a6807decc35e7b1381284c
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-08-14 16:41:45 +0100

    Updated Tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python3
-"""This module contains the definition
-of the `number_of_subscribers()` function
 """
-from requests import get
+Contains the number_of_subscribers function
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns
-    the number of subscribers for a given subreddit
-
-    Args:
-        subreddit(str): The name of the subreddit to query
-
-    Returns:
-        int: The number of subscribers for the given subreddit
-        Return 0 if the subreddit is invalid or if there's an error.
-
-    Note: This function does not follow redirects to ensure invalid
-        subreddit are properly handled.
-    """
-    if subreddit is None or not isinstance(subreddit, str):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,39 +1,22 @@
 #!/usr/bin/python3
-"""
-This module containd the definition of the function
-`top_ten()`
-"""
+"""Contains top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the top 10 hot
-    posts of a given subreddit
-
-    Args:
-        subreddit(int): The name of the subreddit
-
-    Returns:
-        None
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'My Mozilla Browser'}
-    params = {'limit': 10}
-
-    try:
-        response = requests.get(
-                url,
-                headers=headers,
-                params=params,
-                allow_redirects=False
-                )
-        if response.status_code == 200:
-            posts = response.json().get('data').get('children')
-
-            for post in posts:
-                print(post.get('data').get('title'))
-        else:
-            print(None)
-    except Exception:
-        print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
