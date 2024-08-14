4f5cd1a3d2eacad4fdec68d61873eb5c4c5b6b8f
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2024-08-14 16:38:17 +0100

    Updated solution

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# This puppet manifest fixes a server error that was caused by a typo
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-exec {'replate_line':
-  command  => "sed -i 's/phpp/php/g' '/var/www/html/wp-settings.php'",
-  path    => ['/bin', '/usr/bin']
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
